d5e6cc420ef628995206145a33cd9da1e2b8dd34
Mark Seaman <markseaman@iMac>
2016-05-04 09:21:06 -0600

    Create view to list the tasks

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,16 +1,27 @@
 from django.conf.urls import url
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-from tool.views import home
+# from tool.views import home
+#
+# def home(request):
+#         title = "World's Simplest App"
+#         text = '''
+#         This is the simplest Django app that is possible. All extra stuff has
+#         been stripped out. Only essential code remains. .
+#         '''
+#         return HttpResponse("<h1>%s</h1><p>%s</p>" % (title,text))
 
-def home(request):
-        title = "World's Simplest App"
-        text = '''
-        This is the simplest Django app that is possible. All extra stuff has
-        been stripped out. Only essential code remains. .
-        '''
-        return HttpResponse("<h1>%s</h1><p>%s</p>" % (title,text))
+
+from tasks.views import TaskList
 
 urlpatterns = [
-    url(r'^$', home)
+    #url(r'^$', home),
+
+    # Home view
+    #url(r'^$', TemplateView.as_view(template_name="task.html")),
+
+    # List view
+    url(r'^$', TaskList.as_view(), name='contact_list'),
+
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+# from django.shortcuts import render
+
+from tasks.models import Task
+
+
+#-----------------------------------------------------------------------------
+# List view
+
+# Basic list view with using a template
+
+class TaskList(ListView):
+    model = Task
+    template_name = 'task.html'
