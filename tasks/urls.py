9004a7ddaab60ff63de5e61beea870ea090d0c7a
Mark Seaman <markseaman@iMac>
2016-05-05 15:24:35 -0600

    Implement task edit, add, and delete views

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
+from tasks.views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
 
-from tasks.views import TaskList, TaskDetail
 
 urlpatterns = [
 
@@ -11,5 +11,12 @@ urlpatterns = [
     # Detail view
     url(r'^(?P<pk>\d+)$', TaskDetail.as_view(), name='task-detail'),
 
+    # Add view
+    url(r'add$', TaskCreate.as_view(), name='task_add'),
 
+    # Update view
+    url(r'(?P<pk>\d+)/edit$', TaskUpdate.as_view(), name='task_update'),
+
+    # Delete view
+    url(r'(?P<pk>\d+)/delete$', TaskDelete.as_view(), name='task_delete'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,24 +1,40 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
 
 from tasks.models import Task
 
 
-#-----------------------------------------------------------------------------
-# List view
-
-# Basic list view with using a template
-
+# List view:  Basic list view with using a template
 class TaskList(ListView):
     model = Task
     template_name = 'task.html'
 
 
-#-----------------------------------------------------------------------------
-# Detail view
-
-# Basic detail view
+# Detail view:  Basic detail view
 class TaskDetail(DetailView):
-
-
     model = Task
     template_name = 'task_detail.html'
+
+
+# Create view
+class TaskCreate(CreateView):
+    model = Task
+    fields = ['name', 'date', 'notes','hours','done']
+    template_name = 'task_edit.html'
+    success_url = reverse_lazy('task_list')
+
+
+# Update view
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = ['name', 'date', 'notes','hours','done']
+    template_name = 'task_edit.html'
+    success_url = reverse_lazy('task_list')
+
+
+# Delete view
+class TaskDelete(DeleteView):
+    model = Task
+    success_url = reverse_lazy('task_list')
+    template_name = 'task_delete.html'
