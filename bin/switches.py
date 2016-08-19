4ce77f3e5fe681b257c9f2a05e79ab0c8b8bd443
Mark Seaman <seaman@Marks-iMac.local>
2016-08-19 13:53:55 -0600

    Setup Hammer context based on MyBook

--- bin/switches.py
+++ bin/switches.py
@@ -2,19 +2,20 @@ from os import environ
 from os.path import join
 
 
-PROJECT_BASE = join(environ['HOME'],'Projects','w2w')
+PROJECT_BASE = join(environ['HOME'],'Projects','MyBook')
 
 IMPORT_RECORDS = True
+
 EXPORT_RECORDS = True
+
 SHOW_WEB_PAGE = False
+
 ON_INTERNET = True
 
+TEST_DOC = join(environ['p'], 'Documents', 'Index.md')
 
+TODO_DIR  = join(environ['HOME'], 'Projects', 'MyBook', 'Documents', 'tech', 'collab', 'Client')
 
-TEST_DOC = join(environ['p'],'Documents','tech', 'Index.md')
-
-TODO_DIR  = join(environ['p'], 'Documents', 'tech', 'collab', 'Client')
-#TASK_DIR  = join(environ['HOME'], 'Documents', 'MyBook', 'notes', 'Task')
 TODO_FILES = [
     join(TODO_DIR, 'Hammer-ToDo'),
     join(TODO_DIR, 'Hammer-Done'),
@@ -22,21 +23,19 @@ TODO_FILES = [
     join(TODO_DIR, '2016-08.md'),
 ]
 
-APP_PORT = '8001'
-APP_DIRS = ['hammer', 'tool', 'spiritual']
+APP_PORT = '8003'
+
+APP_DIRS = ['hammer', 'tool']
+
 APP_DIR = 'tool'
 
-
-#if environ.has_key('p'):
-#     BASE_DIR = environ['p']
-# else:
-#     from hammer.settings import BASE_DIR
-
 TEST_DOC_ENCODING = False
+
 TEST_DATA = False
 
 TEST_SELENIUM = True
-TEST_PAGES = join(environ['p'], 'Documents', 'tech', 'test', 'pages')
 
-TEST_HOSTS = ['localhost:8001', environ['DROPLET_IP']]
+TEST_PAGES = join(environ['p'], 'Documents', 'test-pages')
+
+TEST_HOSTS = ['localhost:8003', environ['DROPLET_IP']]
 
